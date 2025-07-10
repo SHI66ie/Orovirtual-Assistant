4e3e9c08cc9d277b84245852532fa44ba472f9e8
SHI66ie <ybarkido@gmail.com>
2025-07-10 14:07:55 +0100

    Update configuration for new domain orovirtualassistant.com

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/Orovirtual-Assistant/',
+  base: '/',
   server: {
     port: 3000,
     open: true
